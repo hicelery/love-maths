5eedb9658ad082fdfa341dd7cd85a89ad2c34cb8
hicelery <mungoharrison.ward9@gmail.com>
2025-12-15 11:16:00 +0000

    add js function template

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
 console.log("Hello, Love Maths!");
+
+function runGame() {
+    console.log("Game is running...");
+}
+
+function checkAnswer(answer) {
+    console.log("Checking answer: " + answer);
+}
+
+function calculateCorrectAnswer() {
+    console.log("Calculating correct answer...");
+}
+
+function incrementScore() {
+    console.log("Incrementing score...");
+}
+
+function displayAdditionQuestion() {
+    console.log("Displaying addition question...");
+}
+
+function displaySubtractQuestion() {
+    console.log("Displaying subtraction question...");
+}
+function displayMultiplyQuestion() {}
