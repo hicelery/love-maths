f03ea01b0da3ca19c61bd3e1618aaf21de51f357
hicelery <mungoharrison.ward9@gmail.com>
2025-12-15 15:12:50 +0000

    add js game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,15 +14,29 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     }
+
+    // Start the game for the first time
+    runGame("addition");
 });
 /**
  * The main game loop called when script is first loaded
  * and after the user's answer has been processed.
  */
-function runGame() {
+function runGame(gameType) {
     //create two random numbers between 1 and 25
     let num1 = Math.ceil(Math.random() * 25);
     let num2 = Math.ceil(Math.random() * 25);
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer(answer) {
@@ -37,11 +51,13 @@ function incrementScore() {
     console.log("Incrementing score...");
 }
 
-function displayAdditionQuestion() {
-    console.log("Displaying addition question...");
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
     console.log("Displaying subtraction question...");
 }
-function displayMultiplyQuestion() {}
+function displayMultiplyQuestion(operand1, operand2) {}
