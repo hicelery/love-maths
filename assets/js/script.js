97ed58999bd1db00027943d7fc9916dad76d020f
hicelery <mungoharrison.ward9@gmail.com>
2025-12-15 15:10:27 +0000

    add basic gaem functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,9 +15,14 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
-
+/**
+ * The main game loop called when script is first loaded
+ * and after the user's answer has been processed.
+ */
 function runGame() {
-    console.log("Game is running...");
+    //create two random numbers between 1 and 25
+    let num1 = Math.ceil(Math.random() * 25);
+    let num2 = Math.ceil(Math.random() * 25);
 }
 
 function checkAnswer(answer) {
