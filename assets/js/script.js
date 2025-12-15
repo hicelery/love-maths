408442c6d580ccd1a5d34043e34aafa711023517
hicelery <mungoharrison.ward9@gmail.com>
2025-12-15 16:26:42 +0000

    division function fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -131,7 +131,8 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operator").textContent = "*";
 }
 
-function displayDivideQuestion(operand1, operand2) {
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "/";
